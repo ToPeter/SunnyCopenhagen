a238483d7721628d8b52fcbebaa30bb582652271
tomoetom <tomoemp@gmail.com>
2014-03-19 11:54:03 +0100

    added methods in DB connector. using test DBaccount

--- src/dataSource/DBConnector.java
+++ src/dataSource/DBConnector.java
@@ -6,11 +6,44 @@
 
 package dataSource;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Tomoe
  */
 public class DBConnector
-{
-    
+{// connecting testDB
+  private static final String id = "SEM2_TEST_GR21"; 
+  private static final String pw = "SEM2_TEST_GR21";
+  private static Connection connection;
+
+  private static void intializeConnection() {
+    try {
+      connection = DriverManager.getConnection("jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat", id, pw);
+    } catch (SQLException e) {
+      System.out.println("\n*** Remember to insert your Oracle ID and PW in the DBConnector class! ***\n");
+      System.out.println("Could not create a Connection in DBConnector.getConnection(): " + e);
+    }
+  }
+
+  public static Connection getConnection() {
+    if (connection == null) {
+      intializeConnection();
+    }
+    return connection;
+  }
+
+  public static void releaseConnection(Connection con) {
+    try {
+      connection.close();
+    } catch (SQLException e) {
+      System.err.println(e);
+    }
+  }
 }
+
+    
+
