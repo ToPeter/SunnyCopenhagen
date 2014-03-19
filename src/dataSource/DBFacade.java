b506d3bfdbf89b9f235c905404442f5aa6f29ded
Peter K <millipeter15@hotmail.com>
2014-03-19 12:12:38 +0100

    Just added some to the DB FACADE
    Just addede some minor code to the DBFACADE

--- src/dataSource/DBFacade.java
+++ src/dataSource/DBFacade.java
@@ -6,6 +6,8 @@
 
 package dataSource;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Tomoe
@@ -13,4 +15,67 @@ package dataSource;
 public class DBFacade
 {
     
+    private OrderMapper om;
+    private Connection con;
+
+    //== Singleton start
+    private static DBFacade instance;
+
+    private DBFacade()
+    {
+        om = new OrderMapper();
+        con = new DBConnector().getConnection();  // the connection will be released upon program 
+        // termination by the garbage collector		  
+    }
+
+    public static DBFacade getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new DBFacade();
+        }
+        return instance;
+    }
+	  //== Singleton end
+
+    
+    //== this is gonna be changed to making the reservations / updating and stuff
+    //== i kept is so we could see how we did earlier :) - Peter K
+    
+    
+//    public Order getOrder(int ono)
+//    {
+//        return om.getOrder(ono, con);
+//    }
+//
+//    public boolean saveNewOrder(Order o)
+//    {
+//        return om.saveNewOrder(o, con);
+//    }
+//
+//    public boolean saveNewOrderDetail(OrderDetail od)
+//    {
+//        return om.saveNewOrderDetail(od, con);
+//    }
+//
+//    public boolean updateCurrentOrder(Order o)
+//    {
+//        return om.updateCurrentOrder(o, con);
+//    }
+//
+//    public boolean updateOrderDetail(OrderDetail od)
+//    {
+//        return om.updateOrderDetail(od, con);
+//    }
+//
+//    public boolean deleteOrderDetail(OrderDetail od)
+//    {
+//           return om.deleteOrderDetail(od, con);
+//    }
+//
+//    public void deleteOrder(Order o)
+//    {
+//
+//        om.deleteOrder(o, con);
+//    }
 }
