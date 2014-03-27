710c0eb1d5680d3df10778f0e3dd7d11778b6837
tomoetom <tomoemp@gmail.com>
2014-03-27 14:19:33 +0100

    Mailfunctions update

--- src/MailSender/Mail.java
+++ src/MailSender/Mail.java
@@ -12,7 +12,10 @@ package MailSender;
  */
 
 import com.itextpdf.text.DocumentException;
+import domain.Guest;
+import domain.Reservation;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -34,27 +37,33 @@ import javax.mail.internet.MimeMultipart;
 
 public class Mail {
     
-    public void sendInvoice(int reservationNo, String firstName, String lastName) throws MessagingException {
+    public boolean sendInvoice(Reservation reservation, ArrayList<Guest> guestarray, String roomType, int roomPrice) throws MessagingException {
         
         PDF pdf= new PDF();
-        
+        int reservationNo=reservation.getReservationNo();
+        System.out.println("reservation in Mail" +reservationNo);
+        String firstName=guestarray.get(0).getGuestFirstName();
+   String lastName=guestarray.get(0).getGuestFamilyName();
+   
         try
         {
-            pdf.createPdf(reservationNo, firstName,lastName);
+            pdf.createInvoice(reservation, guestarray, roomType,roomPrice);
         }
         catch (DocumentException ex)
         {
             Logger.getLogger(Mail.class.getName()).log(Level.SEVERE, null, ex);
+        return false;
         }
         catch (IOException ex)
         {
             Logger.getLogger(Mail.class.getName()).log(Level.SEVERE, null, ex);
+        return false;
         }
         
         String host = "smtp.gmail.com";
         String Password = "cphbusiness11";
         String from = "casablancaholidaycentre@gmail.com ";
-        String toAddress = "thehappygolfer@gmail.com";
+        String toAddress = "tomoetom@gmail.com";
         String filename = pdf.getFilePath();
         // Get system properties
         Properties props = System.getProperties();
@@ -102,81 +111,84 @@ public class Mail {
             tr.sendMessage(message, message.getAllRecipients());
             System.out.println("Mail Sent Successfully");
             tr.close();
+            return true;
 
         } catch (SendFailedException sfe) {
 
             System.out.println(sfe);
+            return false;
         }
-    }
-     public void SendMail(int reservationNo, String firstName, String lastName) throws MessagingException {
-        
-        PDF pdf= new PDF();
-        
-        try
-        {
-            pdf.createPdf(reservationNo, firstName,lastName);
-        }
-        catch (DocumentException ex)
-        {
-            Logger.getLogger(Mail.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        catch (IOException ex)
-        {
-            Logger.getLogger(Mail.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        String host = "smtp.gmail.com";
-        String Password = "cphbusiness11";
-        String from = "casablancaholidaycentre@gmail.com ";
-        String toAddress = "Tomoetom@gmail.com,thehappygolfer@gmail.com";
-        String filename = pdf.getFilePath();
-        // Get system properties
-        Properties props = System.getProperties();
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtps.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        Session session = Session.getInstance(props, null);
-
-        MimeMessage message = new MimeMessage(session);
-
-        message.setFrom(new InternetAddress(from));
-
-        message.setRecipients(Message.RecipientType.TO, toAddress);
-
-        message.setSubject("JavaMail Attachment test");
-
-        BodyPart messageBodyPart = new MimeBodyPart();
-
-        messageBodyPart.setText("Tomoe is trying");
-
-        Multipart multipart = new MimeMultipart();
-
-        multipart.addBodyPart(messageBodyPart);
-
-        messageBodyPart = new MimeBodyPart();
-
-        DataSource source = new FileDataSource(filename);
-
-        messageBodyPart.setDataHandler(new DataHandler(source));
-
-        messageBodyPart.setFileName(filename);
-
-        multipart.addBodyPart(messageBodyPart);
-
-        message.setContent(multipart);
-
-        try {
-            Transport tr = session.getTransport("smtps");
-            tr.connect(host, from, Password);
-            tr.sendMessage(message, message.getAllRecipients());
-            System.out.println("Mail Sent Successfully");
-            tr.close();
-
-        } catch (SendFailedException sfe) {
-
-            System.out.println(sfe);
-        }
-    }
+    }    
+//    
+//     public void SendMail(int reservationNo, String firstName, String lastName) throws MessagingException {
+//        
+//        PDF pdf= new PDF();
+//        
+//        try
+//        {
+//            pdf.createPdf(reservationNo, firstName,lastName);
+//        }
+//        catch (DocumentException ex)
+//        {
+//            Logger.getLogger(Mail.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        catch (IOException ex)
+//        {
+//            Logger.getLogger(Mail.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        
+//        String host = "smtp.gmail.com";
+//        String Password = "cphbusiness11";
+//        String from = "casablancaholidaycentre@gmail.com ";
+//        String toAddress = "Tomoetom@gmail.com,thehappygolfer@gmail.com";
+//        String filename = pdf.getFilePath();
+//        // Get system properties
+//        Properties props = System.getProperties();
+//        props.put("mail.smtp.host", host);
+//        props.put("mail.smtps.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        Session session = Session.getInstance(props, null);
+//
+//        MimeMessage message = new MimeMessage(session);
+//
+//        message.setFrom(new InternetAddress(from));
+//
+//        message.setRecipients(Message.RecipientType.TO, toAddress);
+//
+//        message.setSubject("JavaMail Attachment test");
+//
+//        BodyPart messageBodyPart = new MimeBodyPart();
+//
+//        messageBodyPart.setText("Tomoe is trying");
+//
+//        Multipart multipart = new MimeMultipart();
+//
+//        multipart.addBodyPart(messageBodyPart);
+//
+//        messageBodyPart = new MimeBodyPart();
+//
+//        DataSource source = new FileDataSource(filename);
+//
+//        messageBodyPart.setDataHandler(new DataHandler(source));
+//
+//        messageBodyPart.setFileName(filename);
+//
+//        multipart.addBodyPart(messageBodyPart);
+//
+//        message.setContent(multipart);
+//
+//        try {
+//            Transport tr = session.getTransport("smtps");
+//            tr.connect(host, from, Password);
+//            tr.sendMessage(message, message.getAllRecipients());
+//            System.out.println("Mail Sent Successfully");
+//            tr.close();
+//
+//        } catch (SendFailedException sfe) {
+//
+//            System.out.println(sfe);
+//        }
+//    }
 }
-    
-
+//    
+//
--- src/MailSender/PDF.java
+++ src/MailSender/PDF.java
@@ -25,6 +25,12 @@ import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
+import domain.Guest;
+import domain.Reservation;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * First iText example: Hello World.
@@ -33,12 +39,29 @@ public class PDF
 {
 
     String filePath;
-    
 
-    public void createPdf(int reservationNo, String firstName, String lastName)
+    private String toStringForPDF(String roomType, ArrayList<Guest> guestArray)
+    {
+        String guests = "";
+        for (int i = 0; i < guestArray.size(); i++)
+        {
+            guests += guestArray.get(i).toString();
+
+        }
+        return "Room Type: " + roomType + "\n" + guests;
+    }
+
+    public void createInvoice(Reservation reservation, ArrayList<Guest> guestarray, String roomType, int roomPrice)
             throws DocumentException, IOException
-    {filePath="Invoice"+reservationNo+".pdf";
+    {
+        filePath = "Invoice" + reservation.getReservationNo() + ".pdf";
         // step 1
+        String firstName = guestarray.get(0).getGuestFirstName();
+        String lastName = guestarray.get(0).getGuestFamilyName();
+        int deposit = roomPrice / 2;
+
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy");
+
         Document document = new Document();
         System.out.println("document");
         // step 2
@@ -50,20 +73,29 @@ public class PDF
         // step 4
         Font font = FontFactory.getFont("Times-Roman", 18);
         Font fontbold = FontFactory.getFont("Times-Roman", 34, Font.BOLD);
-        
+
         document.add(new Paragraph("Invoice", fontbold));
-document.add(new Paragraph("Casablanca Holiday Centre", font));
+        document.add(new Paragraph("                                    Casablanca Holiday Centre", font));
 
         document.add(new Paragraph(Chunk.NEWLINE));
         document.add(new Paragraph(Chunk.NEWLINE));
-        document.add(new Paragraph("Dear " +firstName+" "+lastName+",", font));
+        document.add(new Paragraph("Dear " + firstName + " " + lastName + ",", font));
         document.add(new Paragraph(Chunk.NEWLINE));
         document.add(new Paragraph("thank you for your reservation.", font));
+        document.add(new Paragraph(Chunk.NEWLINE));
+        document.add(new Paragraph("++++++Reservation details+++++++", font));
+        document.add(new Paragraph("Name: "+guestarray.get(0).getGuestFirstName()+" "+guestarray.get(0).getGuestFamilyName(), font));
+        document.add(new Paragraph("From: " + dateFormat.format(reservation.getFromDate()), font));
+        document.add(new Paragraph("To: " + dateFormat.format(reservation.getEndDate()), font));
+        document.add(new Paragraph("Room type: "+roomType, font));
+        document.add(new Paragraph("++++++++++++++++++++++++++++++++",font));
+
+        document.add(new Paragraph(Chunk.NEWLINE));
 
         document.add(new Paragraph("Please place the deposit in our follwing bank account in 5 days.", font));
         document.add(new Paragraph(Chunk.NEWLINE));
 
-        document.add(new Paragraph("Deposit ammount: xxx", font));
+        document.add(new Paragraph("Deposit ammount: " + deposit + " USD", font));
         document.add(new Paragraph("Bank account: xxxx xxxx xxxx", font));
 
         document.add(new Paragraph(Chunk.NEWLINE));
@@ -82,11 +114,80 @@ document.add(new Paragraph("Casablanca Holiday Centre", font));
         document.add(new Paragraph(Chunk.NEWLINE));
         document.add(new Paragraph("The deposit must be paid at the latest 5 days after receiving the invoice.", font));
         document.add(new Paragraph("The deposit is half the bill. If deposit is not received by the centre within this deadline the booking will be cancelled.", font));
-          document.add(new Paragraph("In case of cancellation your deposit will be refunded after deducting the cancellation fees as follows.", font));
-document.add(new Paragraph(Chunk.NEWLINE));
-                  document.add(new Paragraph("2 weeks before the date of arrival: ½ the deposit will be refunded", font));
-document.add(new Paragraph("1 week before the date of arrival: ¼ the deposit will be refunded", font));
-document.add(new Paragraph("Less than a week no deposit is refunded.", font));
+        document.add(new Paragraph("In case of cancellation your deposit will be refunded after deducting the cancellation fees as follows.", font));
+        document.add(new Paragraph(Chunk.NEWLINE));
+        document.add(new Paragraph("2 weeks before the date of arrival: ½ the deposit will be refunded", font));
+        document.add(new Paragraph("1 week before the date of arrival: ¼ the deposit will be refunded", font));
+        document.add(new Paragraph("Less than a week no deposit is refunded.", font));
+        // step 5
+        document.close();
+        System.out.println("closed");
+    }
+
+    public void createConfirmation(Reservation reservation, ArrayList<Guest> guestarray, String roomType, int roomPrice)
+            throws DocumentException, IOException
+    {
+        filePath = "Invoice" + reservation.getReservationNo() + ".pdf";
+        // step 1
+        String firstName = guestarray.get(0).getGuestFirstName();
+        String lastName = guestarray.get(0).getGuestFamilyName();
+        int deposit = roomPrice / 2;
+
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy");
+
+        Document document = new Document();
+        System.out.println("document");
+        // step 2
+        FileOutputStream fos = new FileOutputStream(filePath);
+        PdfWriter.getInstance(document, fos);
+        System.out.println("fos");
+        // step 3
+        document.open();
+        // step 4
+        Font font = FontFactory.getFont("Times-Roman", 18);
+        Font fontbold = FontFactory.getFont("Times-Roman", 34, Font.BOLD);
+
+        document.add(new Paragraph("Invoice", fontbold));
+        document.add(new Paragraph("Casablanca Holiday Centre", font));
+
+        document.add(new Paragraph(Chunk.NEWLINE));
+        document.add(new Paragraph(Chunk.NEWLINE));
+        document.add(new Paragraph("Dear " + firstName + " " + lastName + ",", font));
+        document.add(new Paragraph(Chunk.NEWLINE));
+        document.add(new Paragraph("thank you for your reservation.", font));
+        document.add(new Paragraph(Chunk.NEWLINE));
+        document.add(new Paragraph("++++++Reservation details+++++++", font));
+        document.add(new Paragraph("From: " + dateFormat.format(reservation.getFromDate()), font));
+        document.add(new Paragraph("To: " + dateFormat.format(reservation.getEndDate()), font));
+        document.add(new Paragraph(toStringForPDF(roomType, guestarray), font));
+
+        document.add(new Paragraph("Please place the deposit in our follwing bank account in 5 days.", font));
+        document.add(new Paragraph(Chunk.NEWLINE));
+
+        document.add(new Paragraph("Deposit ammount: " + deposit + " USD", font));
+        document.add(new Paragraph("Bank account: xxxx xxxx xxxx", font));
+
+        document.add(new Paragraph(Chunk.NEWLINE));
+
+        document.add(new Paragraph("Please read our cancel rule mentioned in the next page", font));
+
+        document.add(new Paragraph(Chunk.NEWLINE));
+
+        document.add(new Paragraph(Chunk.NEWLINE));
+        document.add(new Paragraph("Regards,", font));
+        document.add(new Paragraph("CasaBlanca Hotel Resort", font));
+
+        document.add(new Paragraph(Chunk.NEXTPAGE));
+
+        document.add(new Paragraph("The deposit and rules of cancellations", fontbold));
+        document.add(new Paragraph(Chunk.NEWLINE));
+        document.add(new Paragraph("The deposit must be paid at the latest 5 days after receiving the invoice.", font));
+        document.add(new Paragraph("The deposit is half the bill. If deposit is not received by the centre within this deadline the booking will be cancelled.", font));
+        document.add(new Paragraph("In case of cancellation your deposit will be refunded after deducting the cancellation fees as follows.", font));
+        document.add(new Paragraph(Chunk.NEWLINE));
+        document.add(new Paragraph("2 weeks before the date of arrival: ½ the deposit will be refunded", font));
+        document.add(new Paragraph("1 week before the date of arrival: ¼ the deposit will be refunded", font));
+        document.add(new Paragraph("Less than a week no deposit is refunded.", font));
         // step 5
         document.close();
         System.out.println("closed");
